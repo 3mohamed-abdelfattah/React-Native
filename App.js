8c6cc000246384c9a9b589526ec4160165c8e8b7
MohamedMohamed <m01126165016@icloud.com>
2024-07-29 01:00:27 +0300

    main

---
+++ App.js
@@ -0,0 +1,25 @@
+import { Text, View } from 'react-native';
+
+
+export default function App() {
+  return (
+    <View>
+      <Text>Hello World!!</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    backgroundColor: '#ecf0f1',
+    padding: 8,
+  },
+  paragraph: {
+    margin: 24,
+    fontSize: 18,
+    fontWeight: 'bold',
+    textAlign: 'center',
+  },
+});
